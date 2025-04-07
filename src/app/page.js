33c839a3c33324f134bb09d95f5e24df9bab0cef
Ali3905 <muhammadali30905@gmail.com>
2025-04-07 21:15:09 +0500

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { cookies } from "next/headers";
 import Home from "./home/index"
-=======
-import Home from "../app/home/index"
->>>>>>> 0587404d161a96b8b427e56e87715166f55f8b4f
 
 export default function page() {
   const token = cookies().get("authtoken")?.value || null
